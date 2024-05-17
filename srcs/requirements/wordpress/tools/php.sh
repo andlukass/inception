7c23680600acc22f7e06c4f89221e26cd310c704
andlukass <lopess.andrade@gmail.com>
2024-05-17 13:43:13 +0100

    finish redis cache

--- srcs/requirements/wordpress/tools/php.sh
+++ srcs/requirements/wordpress/tools/php.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-echo "listen = 9000" >> /etc/php/7.4/fpm/pool.d/www.conf
-
-if [ -e "wp-config.php" ]; then
-	echo "wordpress files already exists"
-else
-	wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && \
-	chmod 777 wp-cli.phar && \
-	./wp-cli.phar core download --allow-root && \
-	./wp-cli.phar config create --dbname="wordpress" --dbuser="master" --dbpass="123" --dbhost="mariadb" --dbprefix="wp_" --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root && \
+if ! [ -e "wp-config.php" ]; then
+	wp core download --allow-root && \
+	sleep 15 && \
+	wp config create --dbname="wordpress" --dbuser="master" --dbpass="123" --dbhost="mariadb" --dbprefix="wp_" --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root --extra-php <<PHP
+	define( 'WP_REDIS_HOST', 'redis' );
+	define( 'WP_REDIS_PORT', '6379' );
+	define( 'FS_METHOD', 'direct' );
+PHP
 	chmod 777 wp-config.php && \
-	./wp-cli.phar core install --url="https://127.0.0.1" --title="titulo4" --admin_user="master" --admin_password="123" --admin_email="master@42.fr" --skip-email --allow-root && \
-	./wp-cli.phar user create llopes-d llopes-d@42.fr --role=editor --user_pass=123 --allow-root
+	wp core install --url="https://127.0.0.1" --title="inception" --admin_user="master" --admin_password="123" --admin_email="master@42.fr" --skip-email --allow-root && \
+	wp user create llopes-d llopes-d@42.fr --role=editor --user_pass=123 --allow-root && \
+	wp plugin install https://downloads.wordpress.org/plugin/redis-cache.2.5.2.zip --allow-root
+	wp plugin activate redis-cache --allow-root
+	cp wp-includes/class-wp-object-cache.php ./wp-content/
+	wp redis enable --allow-root
+	wp theme install --activate yuki-reverie-blog --allow-root
 fi
 	php-fpm7.4 -F
