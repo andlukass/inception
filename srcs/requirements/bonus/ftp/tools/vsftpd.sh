9c46b4b01a33a6c3ae75d7b63e151ed4c1e0f221
andlukass <lopess.andrade@gmail.com>
2024-05-26 12:04:09 -0500

    fix upload ftp service

--- srcs/requirements/bonus/ftp/tools/vsftpd.sh
+++ srcs/requirements/bonus/ftp/tools/vsftpd.sh
@@ -2,8 +2,9 @@
 
 echo "local_root=/var/www/html" >> /etc/vsftpd.conf
 echo "pasv_min_port=8000" >> /etc/vsftpd.conf
-echo "pasv_max_port=8000" >> /etc/vsftpd.conf
+echo "pasv_max_port=8030" >> /etc/vsftpd.conf
 echo "pasv_enable=yes" >> /etc/vsftpd.conf
+echo "write_enable=yes" >> /etc/vsftpd.conf
 
 useradd -m -s /bin/bash $MY_USER
 passwd $MY_USER <<EOF
