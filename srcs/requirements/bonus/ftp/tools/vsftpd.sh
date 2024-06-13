0499c84030ce8acd33c9289c7c4f8761e6f28cf2
andlukass <lopess.andrade@gmail.com>
2024-06-13 17:17:09 +0100

    change to ENTRYPOINT and fix ftp

--- srcs/requirements/bonus/ftp/tools/vsftpd.sh
+++ srcs/requirements/bonus/ftp/tools/vsftpd.sh
@@ -6,10 +6,15 @@ echo "pasv_max_port=8040" >> /etc/vsftpd.conf
 echo "pasv_enable=yes" >> /etc/vsftpd.conf
 echo "write_enable=yes" >> /etc/vsftpd.conf
 
+
 useradd -m -s /bin/bash $MY_USER
 passwd $MY_USER <<EOF
 $MY_USER_PW
 $MY_USER_PW
 EOF
+
+service vsftpd stop
+
+chmod 777 -R /var/www/html
 
 vsftpd
